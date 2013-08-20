87c45079ebb7e0a2db4679bafd8b2c2f3a75689f
Andrew Iannaccone <andrewiemail@gmail.com>
2013-08-20 19:11:28 +0000

    Fetch Isotropic logs from Mike McCallister's Amazon WS

---
+++ fetch_iso_logs.py
@@ -0,0 +1,29 @@
+import datetime
+import time
+import requests
+
+day = datetime.datetime.strptime('20130310', '%Y%m%d')
+n_failure = 0
+n_success = 0
+
+while n_success == 0 or n_failure < 30:
+    datestr = day.strftime('%Y%m%d')
+
+    dayurl = 'https://s3.amazonaws.com/static.councilroom.mccllstr.com/'\
+        + 'scrape_data/%s.all.tar.bz2' % datestr
+    print(dayurl)
+    r = requests.get(dayurl)
+    success = r.status_code == 200
+
+    if success:
+        log_file = '/home/ai/test/logs/%s.all.tar.bz2' % datestr
+        f = open(log_file, 'wb')
+        f.write(r.content)
+        f.close()
+        n_failure = 0
+        n_success += 1
+    else:
+        print('No logs for %s' % datestr)
+
+    day = day + datetime.timedelta(days=-1)
+    time.sleep(0.1)
