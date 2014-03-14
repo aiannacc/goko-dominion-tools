d2c4eefffffc7de864f74c5236d6b28131cd1dc1
Andrew Iannaccone <andrewiemail@gmail.com>
2014-03-14 06:32:46 -0700

    Add blank line to log output between fetches

--- start_logwatcher.py
+++ start_logwatcher.py
@@ -191,4 +191,5 @@ if __name__ == '__main__':
                 logging.error(sys.exc_info()[2])
                 pass
             print('Found %d new logs. Checking again in 5 seconds.' % parsecount)
+            print()
             time.sleep(5)
