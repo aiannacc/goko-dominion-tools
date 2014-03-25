5feefa931118f4b3c2f4ca3312a1dc4454dce63e
Andrew Iannaccone <andrewiemail@gmail.com>
2014-03-24 18:20:00 -0700

    Undo previous partial checkin

--- start_servers.py
+++ start_servers.py
@@ -17,7 +17,6 @@ from gdt.automatch.communicator import AutomatchWSH
 from gdt.ratings.leaderboard_handler import LeaderboardHandler
 from gdt.logsearch.logsearch_handler import SearchHandler
 from gdt.kingviz.kingviz_handler import KingdomHandler
-from gdt.logsearch.tourneysearch import TourneySearchHandler
 
 
 class ComprehensiveApplication(tornado.web.Application):
@@ -26,7 +25,6 @@ class ComprehensiveApplication(tornado.web.Application):
             (r"/", SearchHandler),
             (r"/logsearch", SearchHandler),
             (r"/logsearch/", SearchHandler),
-            (r"/tourneysearch", TourneySearchHandler),
             (r"/kingdom", KingdomHandler),
             (r"/kingdom/", KingdomHandler),
             (r"/kingdomvisualize", KingdomHandler),
